da9d9b0f9fc36b7ba81ce79dcadabb9c3a9e4933
Utkarsh Singh <30589139+Utkarsh1999@users.noreply.github.com>
2021-12-02 21:39:55 +0530

    last minute changes, might go kaboom!

--- src/utils/redux.js
+++ src/utils/redux.js
@@ -13,23 +13,68 @@ export const createRequestTypes = (base) => {
 
 export const action = (type, data = {}) => {
   // console.log("TYPE: ",type," Payload: ",data)
-  if (type.includes("REQUEST")) {
-    toast.info("Processing", {
-      toastId: type,
-    });
-  }
 
-  if (type.includes("SUCCESS")) {
-    console.log("action:" + type + "data: " + data);
-    toast.success("Success!!" + data, {
-      toastId: type,
-    });
-  }
-
-  if (type.includes("FAILURE")) {
-    toast.error("Failure!" + data.error, {
-      toastId: type,
-    });
+  switch (type) {
+    case "SIGNIN_REQUEST":
+      toast.info("Signing in...", {
+        toastId: type,
+      });
+      break;
+    case "SIGNIN_SUCCESS":
+      toast.success("Sign In successfully!!", {
+        toastId: type,
+      });
+      break;
+    case "SIGNIN_FAILURE":
+      toast.error("Sign In Failed!!", {
+        toastId: type,
+      });
+      break;
+    case "SIGNUP_REQUEST":
+      toast.info("Signing in...", {
+        toastId: type,
+      });
+      break;
+    case "SIGNUP_SUCCESS":
+      toast.success("SignUp successfully!!", {
+        toastId: type,
+      });
+      break;
+    case "SIGNUP_FAILURE":
+      toast.error("SignUp Failed!!", {
+        toastId: type,
+      });
+      break;
+    case "UPLOAD_IMAGES_REQUEST":
+      toast.info("Uploading Image", {
+        toastId: type,
+      });
+      break;
+    case "UPLOAD_IMAGES_SUCCESS":
+      toast.success("Image Uploaded successfully!!", {
+        toastId: type,
+      });
+      break;
+    case "UPLOAD_IMAGES_FAILURE":
+      toast.error("Uploading image Failed!!", {
+        toastId: type,
+      });
+      break;
+    case "SAVE_ITEM_REQUEST":
+      toast.info("Saving Item...", {
+        toastId: type,
+      });
+      break;
+    case "SAVE_ITEM_SUCCESS":
+      toast.success("Item Saved successfully!!", {
+        toastId: type,
+      });
+      break;
+    case "SAVE_ITEM_FAILURE":
+      toast.error("Oops! something went wrong while saving item!!", {
+        toastId: type,
+      });
+      break;
   }
 
   return { type, payload: data };
